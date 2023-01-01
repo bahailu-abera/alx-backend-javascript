13b3cd1f5ec689309bca1ed0e0c6b4c468f2d8f3
bahailu <aberabahailu4@gmail.com>
2023-01-01 10:14:05 +0300

    handle promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+export default function handleResponseFromAPI(promise) {
+  console.log('Got a response from the API');
+  promise.then(
+    () => ({ status: 200, body: 'success' }),
+    () => new Error(),
+  );
+}
