a0d10e91975ccc380a55935f39ff7428cc61cbe2
bahailu <aberabahailu4@gmail.com>
2023-01-01 12:28:13 +0300

    all settled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const p1 = signUpUser(firstName, lastName);
+  const p2 = uploadPhoto(fileName);
+
+  return Promise.allSettled([p1, p2]);
+}
