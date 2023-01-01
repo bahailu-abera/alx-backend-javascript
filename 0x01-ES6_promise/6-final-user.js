451aa51099478c8f1f82e0250299b6af57a45eec
bahailu <aberabahailu4@gmail.com>
2023-01-01 14:47:10 +0300

    return array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const p1 = signUpUser(firstName, lastName);
-  const p2 = uploadPhoto(fileName);
+  const s = signUpUser(firstName, lastName);
+  const u = uploadPhoto(fileName);
 
-  return Promise.allSettled([p1, p2]);
+  return Promise.allSettled([s, u]).then((result) => {
+    const res = [];
+    result.forEach((val) => {
+      if (val.status === 'fulfilled') {
+        res.push(val);
+      } else {
+        res.push({ status: val.status, value: `Error: ${val.reason.message}` });
+      }
+    });
+    return res;
+  });
 }
