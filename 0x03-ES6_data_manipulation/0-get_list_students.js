2646d2b77fb5907ef3753df683eab5875eefd138
bahailu <aberabahailu4@gmail.com>
2023-01-05 21:56:05 +0300

    spelling correction

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,8 +2,9 @@
  * create and manipulate arrays
  */
 export default function getListStudents() {
-  const students = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'james', location: 'Columbia' },
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   return students;
