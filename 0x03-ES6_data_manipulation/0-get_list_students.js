08b512bfd7da2eb06afc6ca28489014e3bcc84d6
bahailu <aberabahailu4@gmail.com>
2023-01-05 21:08:33 +0300

    create array

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+/**
+ * create and manipulate arrays
+ */
+export default function getListStudents() {
+  const students = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'james', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
+}
