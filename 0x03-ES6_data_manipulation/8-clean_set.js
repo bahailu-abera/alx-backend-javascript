5d1108f0a2bb92ebd853f62fbd5ec6717f174788
bahailu <aberabahailu4@gmail.com>
2023-01-10 01:42:48 +0300

    set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  try {
+    if (startString === '') {
+      return '';
+    }
+    return [...set]
+      .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
+      .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
+      .join('-');
+  } catch (err) {
+    return '';
+  }
+}
