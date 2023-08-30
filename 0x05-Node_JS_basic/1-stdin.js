6899642e0d4fc945ebb404e60d812a45f6b7b004
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-30 12:13:03 +0300

    fix: remove unnecessary event listeners

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-
-  process.stdout.write(`Your name is: ${name}\r`);
-});
-
-process.on('SIGINT', () => {
-  process.stdout.write('This important software is now closing\r');
-  process.exit();
+process.stdin.on('data', (chunck) => {
+  process.stdout.write(`Your name is: ${chunck}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\r');
+  process.stdout.write('This important software is now closing\n');
   process.exit();
 });
