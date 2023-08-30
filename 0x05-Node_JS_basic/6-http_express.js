c03dc9539f31f82be7f7ebd6f17fa28ca0c4a786
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-30 14:52:37 +0300

    fix: set the status code header

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,9 +4,14 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'plain/text');
+
   res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+module.exports = app;
