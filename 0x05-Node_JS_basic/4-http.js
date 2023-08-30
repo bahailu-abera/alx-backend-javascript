a30492a073f3afe4a39217730a0596b6521030cb
Bahailu Abera <aberabahailu4@gmail.com>
2023-08-30 13:37:19 +0300

    fix: use event listeners

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,14 +3,20 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseMessage = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseMessage.length);
+  res.statusCode = 200;
 
-  res.end('Hello Holberton School!');
+  res.write(Buffer.from(responseMessage));
 });
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+module.exports = app;
